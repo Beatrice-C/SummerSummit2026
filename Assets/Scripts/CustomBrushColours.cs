f63ddddf8cc25437d03f5695bd9fc66d46695ea2
Sophia Don Tranho <sophiadnth@gmail.com>
2026-09-06 12:06:36 -0700

    marker size smaller

--- Assets/Scripts/CustomBrushColours.cs
+++ Assets/Scripts/CustomBrushColours.cs
@@ -5,11 +5,22 @@ public class CustomBrushColours : MonoBehaviour
 {
     public DrawingSettings defaultSettings;
 
+    public static int markerWidth = 1;
+
+    public static int eraserWidth = 3;
+
+    private void Start()
+    {
+        // red marker set up
+        defaultSettings.SetMarkerWidth(markerWidth);
+    }
+
     public void SetPenBlack()
     {
         Color c = Color.black;
         c.a = defaultSettings.Transparency;
         defaultSettings.SetMarkerColour(c);
+        defaultSettings.SetMarkerWidth(markerWidth);
         Drawable.drawable.SetPenBrush();
     }
 
@@ -18,6 +29,7 @@ public class CustomBrushColours : MonoBehaviour
         Color c = Color.white;
         c.a = defaultSettings.Transparency;
         defaultSettings.SetMarkerColour(c);
+        defaultSettings.SetMarkerWidth(eraserWidth);
         Drawable.drawable.SetPenBrush();
     }
 }
